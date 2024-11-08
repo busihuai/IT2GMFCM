e6dfd07576ccd1e866a9851c151bf02d4dca8034
busihuai <111437064+busihuai@users.noreply.github.com>
2024-11-08 15:19:05 +0800

    Update IT2GMFCM_rgb_tu.m

--- IT2GMFCM_rgb_tu.m
+++ IT2GMFCM_rgb_tu.m
@@ -2,38 +2,22 @@ clear all;
 close all;
 clc;
 % X = imread('D:\myprogram\FCM\FCM_my\database\0042.tif');
-X = imread('D:\myprogram\FCM\FCM_my\paper3\testresult\airplane35.tif');
+image = imread('D:\myprogram\FCM\FCM_my\airplane35.tif');
 
-[m, n ,p] = size(X);
+[m, n ,p] = size(image);
 X = reshape(double(X),m*n,p);
 
 
 c = 3; max_num = 200;iter = 0;
 N = m*n;
 b1 = 2; b2 = 5;b3 = (b1+b2)/2;
-
-%%
-
-
-for l = 1:1
     
-%     U = abs(randn(m, c));%PŒ™ N––k¡–
-%     U = U./(sum(U, 2)*ones(1, c)); %sum(P,2)«ÛP√ø“ª––µƒ∫Õ
+    U = abs(randn(m, c));%P‰∏∫ NË°åkÂàó
+    U = U./(sum(U, 2)*ones(1, c)); %sum(P,2)Ê±ÇPÊØè‰∏ÄË°åÁöÑÂíå
 
-%      V = abs(rand(c,p));%PŒ™ N––k¡–
-%      V = V.*255;
-     
-%      V = zeros(c, p);%PŒ™ N––k¡–
-%     for i =1:c
-%         fd = find(label_real == i);
-%         if isempty(fd)
-%            break; 
-%         end
-%         V(i,:) = X( randi(numel(fd),1) ,:);
-%     end
-%     
-      [V,U]=fcm(X,c);
-    
+    V = abs(rand(c,p));%P‰∏∫ NË°åkÂàó
+    V = V.*255;
+        
     J_prev = inf; J = [];
     
     for i = 1:max_num
@@ -45,34 +29,7 @@ for l = 1:1
             V1 = V;
         end
         
-%         [U11,~,~]=GMFCM_main(X,label_real,b1);
-%         [U22,~,~]=GMFCM_main(X,label_real,b2);
-        
-
         dist = abs(sum(X.*X, 2)*ones(1, c) + (sum(V.*V, 2)*ones(1, N))'-2*X*V'+eps);
-        
-        
-%         t = (dist).^(1/(b1));
-%         for j = 1:c
-%             t1 = t(:,j);
-%             if j == 1
-%                 t2 = t1;
-%             else
-%                 t2 = t2.*t1;%¿€ª˝
-%             end
-%         end
-%         U1 = (repmat(t2,1,c)).^(1/c)./t;
-%         
-%         t = (dist).^(1/(b2));
-%         for j = 1:c
-%             t1 = t(:,j);
-%             if j == 1
-%                 t2 = t1;
-%             else
-%                 t2 = t2.*t1;%¿€ª˝
-%             end
-%         end
-%         U2 = (repmat(t2,1,c)).^(1/c)./t;
         
         t = (dist).^(1/(b1)); 
         t1 = prod(t,2);
@@ -86,14 +43,10 @@ for l = 1:1
         Udown = min(U1,U2);
         
         t3 = (Uup.*Udown).^(1/2);
-        t3 = t3.^b3; %uij(t+1)µƒm¥Œ∑Ω
+        t3 = t3.^b3; %uij(t+1)ÁöÑmÊ¨°Êñπ
         V = (X'*t3)'./(sum(t3)');%V(0)
         
-        %         Uup=reshape(Uup,m,n,c);
-        %         Udown=reshape(Udown,m,n,c);
-        %     [U,V] = compute_v(m,n,c,V,X,reshape(Uup,m,n,c),reshape(Udown,m,n,c));
-        %     V = V';
-        %% ΩµŒ¨
+
         xx=zeros(c,p);
         yy=zeros(c,p);
         u_new_R=zeros(N,p);
@@ -103,9 +56,7 @@ for l = 1:1
         v11=V;
         v22=V;
         data1=sort(X,1);
-%         data2=reshape(data1,m,n);
-        
-        
+           
         %UR
         comparision=0;
         while (comparision==0)
@@ -187,17 +138,6 @@ for l = 1:1
         V = (V_L+V_R)./2;
         U = (u_new_L1.*u_new_R1).^(1/2);
         
-        
-        %     t3 = (dist./sum(dist,2));
-        %
-        %     Uup(find(t3<1/3)) = U1(find(t3<1/3)); %#ok<FNDSB>
-        %     Uup(find(t3>=1/3)) = U2(find(t3>=1/3)); %#ok<FNDSB>
-        %
-        %
-        %     Udown(find(t3<1/3)) = U2(find(t3<1/3)); %#ok<FNDSB>
-        %     Udown(find(t3>=1/3)) = U1(find(t3>=1/3)); %#ok<FNDSB>
-        
-        %%
         disti =  norm(V1-V, 2)^2;
         if disti < 1e-4
             break;
@@ -209,23 +149,8 @@ for l = 1:1
 
     end 
 
-end
-
-%%
 [~, label] = max(U, [], 2);
 figure
-imshow(uint8(reshape(V(label, :), m, n, p)))
-% imwrite(uint8(reshape(V(label, :), m, n, p)),'D:\myprogram\FCM\FCM_my\paper2\testresult\IT2GMFCM_0005.PNG')
 
-%% gray
-% figure
-% label1=zeros(N,1);
-% label1(label==1) = 1;
-% label1(label==2) = 2;
-% label1(label==3) = 3;
-% imshow(reshape(uint8((label1-1)*255./(k-1)),m,n));
-% imwrite(reshape(uint8((label1-1)*255./(k-1)),m,n),'D:\myprogram\FCM\FCM_my\paper2\testresult\IT2GMFCM_0005.PNG')
-%%
-% [fs,center_p,Num_p,center_lab] = Label_image( imread('7.jpg'),reshape(label,m,n));
-% imshow(fs)
-% imwrite(fs,'D:\myprogram\FCM\FCM_my\paper2\testresult\7_5.PNG')
+[fs,center_p,Num_p,center_lab] = Label_image( image,reshape(label,m,n));
+imshow(fs)
