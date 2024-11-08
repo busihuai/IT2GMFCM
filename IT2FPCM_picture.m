38142eff748487e0e9d478bfded39e260487b7f9
busihuai <111437064+busihuai@users.noreply.github.com>
2024-11-08 15:33:01 +0800

    Update IT2FPCM_picture.m

--- IT2FPCM_picture.m
+++ IT2FPCM_picture.m
@@ -1,4 +1,3 @@
-%function[MIhat,AC,t4,tt]=IT2FPCM(n,D,c,v1,data,data0,gnd)
 clc
 clear all
 close all
@@ -8,36 +7,13 @@ I = imread('D:\myprogram\FCM\FCM_my\database\storagetanks35.tif');
 
 [m, n ,p] = size(I);
 X = reshape(double(I),m*n,p);
-load('D:\myprogram\FCM\FCM_my\paper2\testresult\N44.mat')
-label_real = label;
-label = [];
 
-% data = importdata('Datasets_all30\iris.txt');%K=1.2
-% data0 = importdata('Datasets_all30\iris_label.txt');
 
 
 %%
 c = 3;[n,D]=size(X);
 iter = 0;
-l=10;
-nmi = zeros(l,1);
-ari = zeros(l,1);
-hi = zeros(l,1);
-dbi = zeros(l,1);
-sc = zeros(l,1);
-chi = zeros(l,1);
 
-for l = 1:1
-    u1_u=zeros(n,c);
-    u2_u=zeros(n,c);
-    t1_t=zeros(n,c);
-    t2_t=zeros(n,c);
-    u_up=zeros(n,c);
-    u_down=zeros(n,c);
-    t_up=zeros(n,c);
-    t_down=zeros(n,c);
-    v1_down=zeros(c,D);
-    v1_up=zeros(c,D);
     
 %         [V0,U0]=fcm(X,c);
         V0 = zeros(c, D);%P为 N行k列
@@ -156,49 +132,13 @@ for l = 1:1
 
     [~, label] = max(u',[], 1);
 
-    A = label;
-    B = label_real';
-    [NMI,AMI] = NMI_AMI(A, B);
-    [ARI, RI, MI, ~]=RandIndex(label',label_real);
-    [HI,~,~] = compute_f(label,label_real');
-    DBI = evalclusters(X,label','DaviesBouldin');
-    DBI =DBI.CriterionValues;   
-%     SC = evalclusters(X,label,'gap');
-%     SC =SC.CriterionValues;
-    SC = Eva_Entropy(label,X,D);
-    CHI = evalclusters(X,label','CalinskiHarabasz');
-    CHI =CHI.CriterionValues;
-    
-    nmi(l) = NMI;
-    ari(l) = ARI;  
-    hi(l) = HI;
-    dbi(l) = DBI;
-    sc(l) = SC;
-    chi(l) = CHI;
-    
 end
-nmi = getnum(nmi);
-ari = getnum(ari);
-hi = getnum(hi);
-dbi = getnum(dbi);
-sc = getnum(sc);
-chi = getnum(chi);
 
-%% 写入文件
-xlswrite('demo.xlsx',[nmi,ari,hi,dbi,sc,chi]);
 %% 画图
 [~, label1] = max(u',[], 1);
 [m, n ,p] = size(I);
 figure
 imshow(uint8(reshape(v(label1, :), m, n, p)))
-%imwrite(uint8(reshape(v(label, :), m, n, p)),'D:\myprogram\FCM\FCM_my\paper2\testresult\IT2FPCM_storagetanks35.PNG')
-%% gray
-[m, n ,p] = size(I);
-[~, label] = max(u',[], 1);
-figure
-label1=zeros(m*n,1);
-label1(label==1) = 3;
-label1(label==2) = 1;
-label1(label==3) = 2;
-imshow(reshape(uint8((label1-1)*255./(k-1)),m,n));
-% imwrite(reshape(uint8((label1-1)*255./(k-1)),m,n),'D:\myprogram\FCM\FCM_my\paper2\testresult\IT2FPCM_N44.PNG')
+[fs,center_p,Num_p,center_lab] = Label_image( I,reshape(label1,m,n));
+imshow(fs)
+
